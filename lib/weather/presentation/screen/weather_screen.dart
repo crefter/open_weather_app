30c9bfda04051dcb0b6d45c0cb59fb5be9ac2465
crefter <crefter1@yandex.ru>
2022-09-23 17:07:16 +0700

    create weather screen

---
+++ lib/weather/presentation/screen/weather_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.red,
+        child: const Center(
+          child: Text('Weather screen'),
+        ),
+      ),
+    );
+  }
+}
